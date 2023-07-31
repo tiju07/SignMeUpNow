db80cda13734e794848fce05ff03bce8ccf8b025
Tiju <74893674+tiju07@users.noreply.github.com>
2023-07-31 09:49:41 +0530

    Update conn.js with MongoDB Atlas' URL

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://0.0.0.0:27017/ecommerceOrders", {
+var connURL = "mongodb+srv://tijulukose0402:Tiju123@cluster0.auktjfy.mongodb.net/ecommerceOrders?retryWrites=true&w=majority";
+
+mongoose.connect(connURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("Connection Successful to database")
+    console.log("Connection Successful to database");
 }).catch((err) => {
-    console.log("Error connecting database: " + err)
+    console.log("Error connecting database: " + err);
 })
